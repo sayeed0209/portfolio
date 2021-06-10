54ed1191f0e49ad5fd22e4e18005926be8ee65d3
abu sayeed <sayeed0209@gmail.com>
2021-06-10 19:38:57 +0200

    logo & click event pass by props

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,35 +1,24 @@
 import React from "react";
 import "./Nav.css";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import linkedinIMG from '../../img/linkedin.svg'
 import twitterIMG from '../../img/twitter.svg'
 import githubIMG from '../../img/github.svg'
-import logo from '../../img/sayeed-logo.PNG'
-const NavComponent = () => {
+import logo from '../../img/a.svg'
+import logo2 from '../../img/s.svg'
+const NavComponent = ({onClickShowAbout,onClickShowProject,onClickShowContact}) => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-1 px-5">
-      <Navbar.Brand href="#home"><img src={logo}  width='80px' className="rounded-circle"/></Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3 px-5">
+      <Navbar.Brand href="#home" ><img src={logo}  width='50px' className="rounded-circle" alt="logo"  /> <img src={logo2}  width='50px' className="rounded-circle" alt="logo"/></Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features" >Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets" className='px-4'><img src={linkedinIMG} className="text-light" width="30px"/></Nav.Link>
-          <Nav.Link href="#deets" className='px-4'><img src={twitterIMG} className="text-light" width="30px"/></Nav.Link>
-          <Nav.Link href="#deets" className='px-4'><img src={githubIMG} className="text-light" width="30px"/></Nav.Link>
+        <Nav className="ml-auto">
+        <Nav.Link href="#features" className='px-4 btn btn-outline-warning text-white' onClick={onClickShowAbout}>About</Nav.Link>
+          <Nav.Link href="#pricing" className='px-4' onClick={onClickShowProject}>Projects</Nav.Link>
+          <Nav.Link href="#pricing" className='px-4'onClick={onClickShowContact}>Contact</Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/sayeed0209/" className='px-4'><img src={linkedinIMG} className="text-light" width="30px" alt="linkedin" /></Nav.Link>
+          <Nav.Link href="#deets" className='px-4'><img src={twitterIMG} className="text-light" width="30px"alt="twitter"/></Nav.Link>
+          <Nav.Link href="#deets" className='px-4'><img src={githubIMG} className="text-light" width="30px"alt="github"/></Nav.Link>
          
         </Nav>
       </Navbar.Collapse>

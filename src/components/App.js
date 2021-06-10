711f111c7fe7d1ba0176674ff5780316d0928167
abu sayeed <sayeed0209@gmail.com>
2021-06-10 19:45:24 +0200

    click event fcuntionalities & using usestate to the the App component  set different page navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import  NavComponent from '../components/nav/Nav'
+import About from '../components/about/About'
+import Project from '../components/project/Project'
+import Contact from '../components/contact/Contact'
 const App =()=>{
-    return <div>
-        < NavComponent/>
-    </div>
+    const [ShowSection, setShowSection] = useState(About)
+    const onClickShowAbout=()=>{
+        setShowSection(About)
+    }
+    const onClickShowProject=()=>{
+        setShowSection(Project)
+    }
+    const onClickShowContact=()=>{
+        setShowSection(Contact)
+    }
+
+    return (<div>
+        < NavComponent onClickShowAbout={onClickShowAbout} onClickShowProject={onClickShowProject} onClickShowContact={onClickShowContact}/>
+        {ShowSection}
+        
+    </div>)
 }
 
 export default App;

a3ad9052dc5a889de1b9b72af1afda4ef668404c
abu sayeed <sayeed0209@gmail.com>
2021-06-12 11:49:25 +0200

    animation and responsive layout for the about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import profileIMG from "../../img/profile.png";
+import percentageWhite from "../../img/percentage.PNG";
+import './About.css'
 const About = () => {
   return (
-    <Container className="pt-2">
-      <Row>
-        <Col>
-          <h1 className="display-2">About</h1>
+    <div className="bg-light"> 
+       <div className="shadow-sm p-5 mb-4 bg-white" >
+      <div className="d-flex">
+        <Col className="move-right">
+          <h1 className="display-2 font-weight-bold about">About</h1>
           <h2 className="lead my-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
@@ -19,7 +22,7 @@ const About = () => {
             versions of Lorem Ipsum
           </p>
         </Col>
-        <Col sm={12} md={6} lg={5}>
+        <Col sm={12} md={6} lg={5} className="move-left">
           <img
             src={profileIMG}
             alt="background"
@@ -27,9 +30,9 @@ const About = () => {
             className="ml-auto d-block"
           />
         </Col>
-      </Row>
+      </div>
       <hr />
-      <Row className="justify-content-center">
+      <div className="d-flex justify-content-center">
         <Col>
           <img
             src={profileIMG}
@@ -49,8 +52,42 @@ const About = () => {
         <Col>
           <img src={profileIMG} alt="background" className="img-thumbnail" />
         </Col>
-      </Row>
-    </Container>
+      </div>
+      </div>
+     {/*  */}
+     <Container>
+      <Row className="justify-content-center align-items-stretch"> 
+      <Col sm={12} md={4} className="d-flex flex-column justify-content-between">
+        <h1 className="text-break display-4">Backend</h1>
+         <ul className="list-unstyled p-2 lead">
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+        
+        
+        
+         
+         </ul>
+      </Col>
+      <Col sm={12} md={4}>
+          <img src={percentageWhite} alt="background" width="100%"  className="px-4"/>
+        </Col>
+        <Col sm={12} md={4} className="d-flex flex-column justify-content-between text-right">
+        <h1 className="text-break display-4 ">Front-end</h1>
+        <ul className="list-unstyled  p-2 lead">
+        <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         <li >lorem  lorem loremmlorem </li>
+         
+         </ul>
+        </Col>
+       </Row>
+       </Container>
+    </div>
   );
 };
 

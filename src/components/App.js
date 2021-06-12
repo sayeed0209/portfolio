5c5c631fbb4e0a5a1bb4abb1d49ab4919f67e3c9
abu sayeed <sayeed0209@gmail.com>
2021-06-12 12:57:55 +0200

    onRenderd set as a default value false & on useEffect onRendered set value as true which is use for the nav component for the rendering animation  on each componenet change

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import  NavComponent from '../components/nav/Nav'
 import About from '../components/about/About'
 import Project from '../components/project/Project'
 import Contact from '../components/contact/Contact'
 const App =()=>{
     const [ShowSection, setShowSection] = useState(About)
+    const [isRendered,setIsRendered] = useState(false)
     const onClickShowAbout=()=>{
         setShowSection(About)
     }
@@ -14,8 +15,16 @@ const App =()=>{
     const onClickShowContact=()=>{
         setShowSection(Contact)
     }
+    const onRendered=()=>{
+        setIsRendered (false)
+    }
+ 
+    useEffect(()=>{ 
+    setIsRendered (true)
+    },[ShowSection])
+
 const props = {
-    onClickShowAbout,onClickShowProject,onClickShowContact
+    onClickShowAbout,onClickShowProject,onClickShowContact,isRendered,onRendered
 }
     return (<div>
         < NavComponent props={props}/>
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,24 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 import { Navbar, Nav } from "react-bootstrap";
-import linkedinIMG from '../../img/linkedin.svg'
-import twitterIMG from '../../img/twitter.svg'
-import githubIMG from '../../img/github.svg'
-import logo from '../../img/a.svg'
-import logo2 from '../../img/s.svg'
-const NavComponent = ({props:{onClickShowAbout,onClickShowProject,onClickShowContact}}) => {
+import linkedinIMG from "../../img/linkedin.svg";
+import twitterIMG from "../../img/twitter.svg";
+import githubIMG from "../../img/github.svg";
+import logo from "../../img/a.svg";
+import logo2 from "../../img/s.svg";
+const NavComponent = ({
+  props: {
+    onClickShowAbout,
+    onClickShowProject,
+    onClickShowContact,
+    isRendered,
+    onRendered,
+  },
+}) => {
+  const [navbarClass, setNavbarClass] = useState(`p-3 px-5 move-down`);
+  useEffect(() => {
+    if (isRendered) {
+      setNavbarClass(`p-3 px-5 move-down`);
+      onRendered();
+      setTimeout(() => {
+        setNavbarClass("p-3 px-5");
+      }, 500);
+    }
+  },[isRendered,onRendered]);
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3 px-5 move-down">
-      <Navbar.Brand href="#home" ><img src={logo}  width='50px' className="rounded-circle" alt="logo"  /> <img src={logo2}  width='50px' className="rounded-circle" alt="logo"/></Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className={navbarClass}
+    >
+      <Navbar.Brand href="#home">
+        <img src={logo} width="50px" className="rounded-circle" alt="logo" />{" "}
+        <img src={logo2} width="50px" className="rounded-circle" alt="logo" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-        <Nav.Link className='px-4 btn btn-outline-warning text-white' onClick={onClickShowAbout}>About</Nav.Link>
-          <Nav.Link href="#pricing" className='px-4' onClick={onClickShowProject}>Projects</Nav.Link>
-          <Nav.Link href="#pricing" className='px-4'onClick={onClickShowContact}>Contact</Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/sayeed0209/" className='px-4'><img src={linkedinIMG} className="text-light" width="30px" alt="linkedin" /></Nav.Link>
-          <Nav.Link href="#deets" className='px-4'><img src={twitterIMG} className="text-light" width="30px"alt="twitter"/></Nav.Link>
-          <Nav.Link href="#deets" className='px-4'><img src={githubIMG} className="text-light" width="30px"alt="github"/></Nav.Link>
+          <Nav.Link
+            className="px-4 btn btn-outline-warning text-white"
+            onClick={onClickShowAbout}
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            href="#pricing"
+            className="px-4"
+            onClick={onClickShowProject}
+          >
+            Projects
+          </Nav.Link>
+          <Nav.Link
+            href="#pricing"
+            className="px-4"
+            onClick={onClickShowContact}
+          >
+            Contact
+          </Nav.Link>
+          <Nav.Link
+            href="https://www.linkedin.com/in/sayeed0209/"
+            className="px-4"
+          >
+            <img
+              src={linkedinIMG}
+              className="text-light"
+              width="30px"
+              alt="linkedin"
+            />
+          </Nav.Link>
+          <Nav.Link href="#deets" className="px-4">
+            <img
+              src={twitterIMG}
+              className="text-light"
+              width="30px"
+              alt="twitter"
+            />
+          </Nav.Link>
+          <Nav.Link href="#deets" className="px-4">
+            <img
+              src={githubIMG}
+              className="text-light"
+              width="30px"
+              alt="github"
+            />
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
